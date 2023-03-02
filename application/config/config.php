9e9f9ad63b6f714ac70a5f39cb289e49c0892301
izmal91 <izmal.zainal@razer.com>
2023-03-02 18:44:53 +0800

    add theme sbadmin 2

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = getenv('BASEURL');//'http://localhost:1111/ci/';
+$config['base_url'] = 'http://localhost:1111/ci3_example_code/';
 
 /*
 |--------------------------------------------------------------------------
